246f9c4c9144c48ed5614d34dbcbb5b5b0021379
MasahiroHayashi <catfish.m@nifty.com>
2020-06-15 20:47:07 +0900

    Create accountData.php

---
+++ accountData.php
@@ -0,0 +1,41 @@
+<?php
+$admin_info = array (
+  0 => 
+  array (
+    'id' => 'admin123',
+    'pass' => '6943123d0713ef6bc84382b86dda7288f2418f38c977474de3634fa678f7bfd5',
+    'name' => '管理者',
+    'mail' => 'catfish.m@nifty.com',
+    'rock' => 0,
+  ),
+) ;
+$user_info = array (
+) ;
+$site_setting = array (
+  0 => 
+  array (
+    's0_siteName' => 'オープンデータプラットフォーム　Yo-KAN',
+    's1_backgroundImg' => 'sample_background.jpg',
+    's2_snsText' => '高速で安全で簡単でおしゃれで経済的なオープンデータプラットフォーム Yo-KAN へようこそ。',
+    's3_snsImg' => 'sample_fb_tw_img.jpg',
+    's4_headerName' => '',
+    's5_headerBanner' => 'sample_logo.png',
+    's6_contentTextTop' => '高速で安全で簡単でおしゃれで経済的なオープンデータプラットフォーム <b>Yo-KAN</b>（よーかん）へようこそ。<br><br>
+Yo-KAN は CKAN や DKAN と同じような、データ公開のためのオープンソースプログラムです。Yo-KAN を利用すれば、このサイトのようなWordPress風のデータストアが実に簡単に作成できます。<br><br>
+
+Yo-KAN は 無料レンタルサーバーでも超余裕で動作します。<br>
+　<a href="http://yokan.php.xdomain.jp/" target="_blank">無料レンタルサーバー XFREE に設置したデモ</a><br>
+　<a href="http://yokan.starfree.jp/" target="_blank">スターサーバーフリー に設置したデモ</a><br>
+　<a href="https://ss1.xrea.com/yookan.s1010.xrea.com/" target="_blank">XREA Free（無料プラン）に設置したデモ（SSL対応）</a><br><br>
+Yo-KAN が目指すところ。それは小規模の自治体や企業、NPO、シビックテック等が、自前で構築し自力で運用できるオープンデータサイト。<br>
+費用面や技術力の問題によりCKANやDKANを導入するのが難しいと考えている自治体をはじめ、オープンデータの公開を検討しているすべての方に、選択肢のひとつとして検討していただければ幸いです。<br>
+導入方法の詳細は <a href="">GitHub</a> をご覧ください。<br>
+導入にあたりご相談などがあれば作者まで気軽に<a href="index_mail.php">お問い合わせ</a>ください。',
+    's7_contentTextBottom' => 'Yo-KAN は CC0 <a href="https://creativecommons.org/publicdomain/zero/1.0/deed.ja" target="_blank"><img src="img/cc0.png"/ width="90px;"></a> で提供いたします。<br>
+つまり、この作品に関する著作権法上認められるすべての権利（隣接する権利を含む）を、法令上認められる最大限の範囲で放棄して、パブリック・ドメインに供するということです。たとえ営利目的であっても、許可を得ずに複製、改変が可能です。クレジット表記も不要です。',
+    's8_footerText' => '2020<br>
+Masahiro Hayashi',
+    's9_maxDisplayData' => '10',
+  ),
+) ;
+?>
