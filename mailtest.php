1e905c8515f60141221b205647a6538e441ddb38
MasahiroHayashi <catfish.m@nifty.com>
2020-06-16 11:48:33 +0900

    mail test

---
+++ mailtest.php
@@ -0,0 +1,16 @@
+<?php
+require 'vendor/autoload.php';
+$email = new \SendGrid\Mail\Mail();
+$email->setFrom("mirko@mirko.jp", "送信者A");
+$email->setSubject("TestMail漢字");
+$email->addTo("catfish.m@nifty.com", "受信者B");
+$email->addContent("text/plain", "日本語 English");
+$sendgrid = new \SendGrid(getenv('SENDGRID_API_KEY'));
+try {
+    $response = $sendgrid->send($email);
+    print $response->statusCode() . "\n";
+    print_r($response->headers());
+    print $response->body() . "\n";
+} catch (Exception $e) {
+    echo 'Caught exception: '. $e->getMessage() ."\n";
+}
